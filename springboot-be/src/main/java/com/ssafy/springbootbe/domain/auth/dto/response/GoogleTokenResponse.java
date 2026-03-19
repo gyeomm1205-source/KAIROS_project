cc2b3014596fe0fcd7cdd53102bd5f2e964018b9
dbalsend@naver.com <dbalsend@naver.com>
2026-03-19 17:50:42 +0900

    [fix/S14P21A506-175] import 에러 수정

--- springboot-be/src/main/java/com/ssafy/springbootbe/domain/auth/dto/response/GoogleTokenResponse.java
+++ springboot-be/src/main/java/com/ssafy/springbootbe/domain/auth/dto/response/GoogleTokenResponse.java
@@ -1,6 +1,6 @@
 package com.ssafy.springbootbe.domain.auth.dto.response;
 
-import tools.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
