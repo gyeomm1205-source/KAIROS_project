504f439dfe33f1c11c4cfdb56e505a499d9ad41d
최진우 <jk62362@mensakorea.org>
2026-03-22 20:39:08 +0900

    [feat/S14P21A506-92] 성장 일지 조회 및 분석 완료 콜백 API 구현

--- springboot-be/src/main/java/com/ssafy/springbootbe/persistence/techstack/repository/TechStackRepository.java
+++ springboot-be/src/main/java/com/ssafy/springbootbe/persistence/techstack/repository/TechStackRepository.java
@@ -4,9 +4,12 @@ import com.ssafy.springbootbe.persistence.techstack.entity.TechStack;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface TechStackRepository extends JpaRepository<TechStack, Long> {
 
     // 온보딩 메타 조회 시 전체 기술 스택 목록
     List<TechStack> findAllByOrderByTechNameAsc();
+
+    Optional<TechStack> findByTechName(String techName);
 }
