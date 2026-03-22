e66648e140c98795716460070f6bfec0b0853ac3
최진우 <jk62362@mensakorea.org>
2026-03-23 00:35:59 +0900

    [feat/S14P21A506-165] 커리큘럼 노드 상세 조회 및 AI 추천 근거 조회 API 구현

--- springboot-be/src/main/java/com/ssafy/springbootbe/domain/curricula/service/CurriculaService.java
+++ springboot-be/src/main/java/com/ssafy/springbootbe/domain/curricula/service/CurriculaService.java
@@ -1,4 +1,11 @@
 package com.ssafy.springbootbe.domain.curricula.service;
 
+import com.ssafy.springbootbe.domain.curricula.dto.response.CurriculumNodeResponse;
+import com.ssafy.springbootbe.domain.curricula.dto.response.CurriculumReasonResponse;
+
 public interface CurriculaService {
+
+    CurriculumReasonResponse getReason(Long userId, Long curriculumId);
+
+    CurriculumNodeResponse getNode(Long userId, Long curriculumNodeId);
 }
