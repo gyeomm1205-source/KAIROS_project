32ce1158bea36139a9a7856b13d8025fb2b7f435
Mayonaise <genjinie7@gmail.com>
2026-03-24 21:56:30 +0900

    [be/fix] security config 원복

--- springboot-be/src/main/java/com/ssafy/springbootbe/common/config/SecurityConfig.java
+++ springboot-be/src/main/java/com/ssafy/springbootbe/common/config/SecurityConfig.java
@@ -49,8 +49,8 @@ public class SecurityConfig {
                 .exceptionHandling(exception -> exception.authenticationEntryPoint(authenticationFailureHandler))
                 .addFilterBefore(verificationFilter, UsernamePasswordAuthenticationFilter.class)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/v1/auth/**").permitAll()
-                        .requestMatchers("/api/v1/analysis/complete").permitAll()
+                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/analysis/complete").permitAll()
                         .anyRequest().authenticated());
         return http.build();
     }
