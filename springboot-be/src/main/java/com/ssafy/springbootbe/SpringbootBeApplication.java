2c9e6aebf2ea706b545a6c59096351cbe5393b00
Mayonaise <genjinie7@gmail.com>
2026-03-29 07:20:59 +0900

    [be/chore] 서비스 시간 한국시간으로 정리

--- springboot-be/src/main/java/com/ssafy/springbootbe/SpringbootBeApplication.java
+++ springboot-be/src/main/java/com/ssafy/springbootbe/SpringbootBeApplication.java
@@ -4,11 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableAsync
 public class SpringbootBeApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
         SpringApplication.run(SpringbootBeApplication.class, args);
     }
 
